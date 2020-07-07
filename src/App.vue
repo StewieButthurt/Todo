7c318bcf34bd963253e7c26fdab0d25df77cc00e
StewieButthurt <nosoff.slawa@yandex.ru>
2020-07-07 22:17:02 +0400

    Подготовка webpack и vue к разработке. Добавление необходимых модулей.

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="test">
+
+    </div>
+    
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="sass">
+    .test 
+        display: flex
+        flex-direction: column
+</style>
---
+++ webpack.config.js
@@ -0,0 +1,146 @@
+const path = require('path')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const { HotModuleReplacementPlugin } = require('webpack')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCssAssetWebpackPlugin = require('optimize-css-assets-webpack-plugin')
+const TerserWebpackPlugin = require('terser-webpack-plugin')
+
+
+const isDev = process.env.NODE_ENV === 'development'
+const isProd = !isDev
+
+const filename = ext => isDev ? `[name].${ext}` : `[name].[hash].${ext}`
+
+const optimization = () => {
+    const config = {
+        splitChunks: {
+            chunks: 'all'
+        }
+    }
+
+    if (isProd) {
+        config.minimizer = [
+            new OptimizeCssAssetWebpackPlugin(),
+            new TerserWebpackPlugin()
+        ]
+    }
+    return config
+}
+
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        main: ['@babel/polyfill', path.resolve(__dirname, 'src', 'main.js')]
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist', 'build'),
+        filename: 'bundle.js'
+    },
+    optimization: optimization(),
+    devServer: {
+        port: 3000,
+        hot: isDev
+    },
+    devtool: isDev ? 'source-map' : '',
+    module: {
+        rules: [
+
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+            {
+                test: /\.css$/,
+                // лоадеры считываются справо-налево
+                use: [
+
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            hmr: isDev,
+                            reloadAll: true
+                        },
+                    },
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.sass$/,
+                use: [
+                    'vue-style-loader',
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            hmr: isDev,
+                            reloadAll: true
+                        },
+                    },
+                    'css-loader',
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sassOptions: {
+                                indentedSyntax: true
+                            }
+                        }
+                    }
+
+                ]
+            },
+            {
+                test: /\.(png|jpg|svg|gif)$/,
+                use: ['file-loader']
+            },
+            // настройка подключения шрифтов
+            {
+                test: /\.(ttf|woff|woff2|eot)$/,
+                use: ['file-loader']
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env']
+                }
+            }
+
+        ]
+    },
+    resolve: {
+        alias: {
+            'vue$': 'vue/dist/vue.esm.js',
+            '~': path.resolve(__dirname, 'src')
+        }
+    },
+    plugins: [
+        new HotModuleReplacementPlugin(),
+        new VueLoaderPlugin(),
+        new HTMLWebpackPlugin({
+            showErrors: true,
+            cache: true,
+            template: path.resolve(__dirname, 'public', 'index.html'),
+            minify: {
+                collapseWhitespace: isProd
+            }
+        }),
+        new CleanWebpackPlugin(),
+        new CopyWebpackPlugin({
+            patterns: [
+
+                {
+                    from: path.resolve(__dirname, 'src', 'assets', 'study.ico'),
+                    to: path.resolve(__dirname, 'dist')
+                }
+
+            ]
+        }),
+        new MiniCssExtractPlugin({
+            filename: filename('css')
+        })
+    ]
+
+}
