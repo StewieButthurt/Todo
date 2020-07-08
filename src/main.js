411e6dac0cad061f66f446f988ae09616fe7285a
StewieButthurt <nosoff.slawa@yandex.ru>
2020-07-08 14:56:50 +0400

    Надстройка vuex

--- src/main.js
+++ src/main.js
@@ -4,10 +4,12 @@ import '~/assets/fonts/roboto.css'
 import Vue from "vue";
 import Vuex from 'vuex';
 import App from "./App.vue";
+import store from './store'
 
 Vue.use(Vuex);
 
 new Vue({
     el: "#app",
+    store: store,
     render: h => h(App)
 });
