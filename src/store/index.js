4a22b616d9a990c2b514126ed52abc428e674284
StewieButthurt <nosoff.slawa@yandex.ru>
2020-07-09 21:21:25 +0400

    Добавлен плагин для store, при перезагрузке страницы данные будут сохраняться

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import createPersistedState from "vuex-persistedstate"
 import todoList from './modules/todoList'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+    plugins: [createPersistedState()],
     modules: {
         todoList
     }
