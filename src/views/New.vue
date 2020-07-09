b82381affcb01a73d3257eef5a75daf76f9de318
StewieButthurt <nosoff.slawa@yandex.ru>
2020-07-09 13:58:17 +0400

    Изменения в верстке

--- src/views/New.vue
+++ src/views/New.vue
@@ -1,6 +1,13 @@
 <template>
-    <div class="todo__wrapper">
-
+    <div class="todo__container">
+        <div class="todo__header">
+            <div class="todo__header-title">
+                Todo List
+            </div>
+        </div>
+        <div class="todo__wrapper">
+            
+        </div>
     </div>
 </template>
 
