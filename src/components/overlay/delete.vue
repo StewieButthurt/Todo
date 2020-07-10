bc9403c7db6f2f72b91337b2b00c3995337349f9
StewieButthurt <nosoff.slawa@yandex.ru>
2020-07-10 14:54:29 +0400

    Расширен функционал компонента overlay/delete. Добавлена анимация

--- src/components/overlay/delete.vue
+++ src/components/overlay/delete.vue
@@ -1,15 +1,30 @@
 <template>
-    <div class="overlay-delete">
-        <div class="overlay-delete__question">
+    <div class="overlay-delete" 
+        :class="{
+            'overlay-delete_fade-on' : question === true,
+            'overlay-delete_fade-off' : question === false
+        
+        }"
+        >
+        <div class="overlay-delete__question"
+            :class="{
+                'overlay-delete__question_fade-on' : question === true,
+                'overlay-delete__question_fade-off' : question === false
+            }"
+        >
             <div class="overlay-delete__question-text">
                 Вы точно хотите удалить заметку? 
             </div>
             <div class="overlay-delete__question-buttons">
-                <div class="overlay-delete__button">
+                <div class="overlay-delete__button"
+                    @click="clickOverlayCancel()"
+                >
                     Отменить
                 </div>
-                <div class="overlay-delete__button overlay-delete__button-delete">
-                    Отменить
+                <div class="overlay-delete__button overlay-delete__button-delete"
+                    @click="clickOverlayDelete()"
+                >
+                    Удалить
                 </div>
             </div>
         </div>
@@ -18,7 +33,32 @@
 
 <script>
     export default {
-        
+        props: [
+            'question'
+        ],
+        methods: {
+            async clickOverlayCancel() {
+                this.$emit('clickOverlayCancel')
+            },
+            async clickOverlayDelete() {
+                this.$emit('clickOverlayDelete')
+            }
+        },
+        data() {
+            return {
+                status: false
+            }
+        },
+        watch: {
+            question(val) {
+                if(!val) {
+                    setTimeout(() => {
+
+                        this.status = false 
+                    }, 200);
+                }
+            }
+        }
     }
 </script>
 
@@ -30,10 +70,33 @@
         position: fixed
         top: 0px
         left: 0px   
-        width: 100vw
-        height: 100vh
+        width: 100%
+        height: 100%
+        opacity: 0
         background: rgba(50,50,50,.2)
-        z-index: 100
+        z-index: -5
+    
+    .overlay-delete_fade-on
+        animation: overlay-delete_fade-on .3s forwards
+    
+    @keyframes overlay-delete_fade-on
+        0% 
+            z-index: -5
+            opacity: 0
+        100%
+            z-index: 100
+            opacity: 1
+    
+    .overlay-delete_fade-off
+        animation: overlay-delete_fade-off .3s forwards
+    
+    @keyframes overlay-delete_fade-off
+        0%
+            z-index: 100
+            opacity: 1
+        100%
+            z-index: -5
+            opacity: 0
     
     .overlay-delete__question
         display: flex
@@ -41,12 +104,36 @@
         align-items: center
         background-color: white
         border-radius: 4px
+        transform: scale(0)
+        opacity: 0
         max-width: 400px
         width: 100%
         padding: 40px 40px 20px
         box-shadow: 0 10px 20px 0 rgba(0,0,0,.2)
         +xs-block
             width: 85%
+    
+    .overlay-delete__question_fade-on
+        animation: question-fade-on .3s forwards
+
+    @keyframes question-fade-on
+        0% 
+            opacity: 0
+            transform: scale(0)
+        100%
+            opacity: 1
+            transform: scale(1)
+
+    .overlay-delete__question_fade-off
+        animation: question-fade-off .3s forwards
+    
+    @keyframes question-fade-off
+        0%  
+            opacity: 1
+            transform: scale(1)
+        100%
+            opacity: 0
+            transform: scale(0)
     
     .overlay-delete__question-buttons
         display: flex
@@ -59,6 +146,7 @@
     
     .overlay-delete__button
         display: flex
+        user-select: none
         justify-content: center
         align-items: center
         padding: 0px 15px
