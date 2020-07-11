95b66a99f7eea1243dce68042d0ca4e5bc7d1245
StewieButthurt <nosoff.slawa@yandex.ru>
2020-07-11 16:23:02 +0400

    Добавлен и включен в список модулей, новый модуль editNote

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,14 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import createPersistedState from "vuex-persistedstate"
 import todoList from './modules/todoList'
+import editNote from './modules/editNote'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
     plugins: [createPersistedState()],
     modules: {
-        todoList
+        todoList,
+        editNote
     }
 })
