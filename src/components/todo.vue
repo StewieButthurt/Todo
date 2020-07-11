33e53bb1b7e5f13ca2675ed88e725bf41a255ff3
StewieButthurt <nosoff.slawa@yandex.ru>
2020-07-11 17:41:36 +0400

    Добавлены стили и динамический класс по условию

--- src/components/todo.vue
+++ src/components/todo.vue
@@ -6,7 +6,9 @@
         >
             <svg v-if="status" viewBox="0 0 14 14" class="check"><polygon points="5.5 11.9993304 14 3.49933039 12.5 2 5.5 8.99933039 1.5 4.9968652 0 6.49933039"></polygon></svg>
         </div>
-        <div class="todo-component__title">
+        <div class="todo-component__title"
+            :class="{'todo-component__title-decoration' : status}"
+        >
             {{title}}
         </div>
     </div>
@@ -53,11 +55,16 @@
         align-items: center
         min-width: 16px
         height: 16px
-        border: 1px solid black
+        border: 1px solid #202124
+        border-radius: 2px
         fill: black
-        
+        opacity: .8
+
+    .todo-component__checkbox:hover    
+        opacity: 1
     
     .todo-component__checkbox-active
+        opacity: 1
         background-color: rgb(46, 170, 220)
     
     .todo-component__checkbox svg
@@ -70,4 +77,8 @@
         display: block
         white-space: nowrap
         overflow: hidden
+        color: #202124
+    
+    .todo-component__title-decoration
+        text-decoration: line-through
 </style>
