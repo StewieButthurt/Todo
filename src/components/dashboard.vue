193e5fa4be0e7d4abcd0a0e471fec0967fb07d01
StewieButthurt <nosoff.slawa@yandex.ru>
2020-07-12 16:14:39 +0400

    Добавлен новый компонент содержащий панель управления

---
+++ src/components/dashboard.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="dashboard"
+        :style="{
+            position: `${position}`,
+            left: `${left}`, 
+            top: `${top}`, 
+            right: `${right}`,
+            bottom: `${bottom}`,
+            width: `${width}`,
+            height: `${height}`
+        }"
+    >
+
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            left: {
+                type: String,
+                default: 'auto'
+            },
+            top: {
+                type: String,
+                default: 'auto'
+            },
+            right: {
+                type: String,
+                default: 'auto'
+            },
+            bottom: {
+                type: String,
+                default: 'auto'
+            },
+            position: {
+                type: String,
+                default: 'fixed'
+            },
+            width: {
+                type: String,
+                default: '0px'
+            },
+            height: {
+                type: String,
+                default: '0px'
+            }
+        }
+    }
+</script>
+
+<style lang="sass">
+    .dashboard
+        display: flex
+        flex-direction: column
+        align-items: center
+        justify-content: space-around
+        background: #383838
+        border-radius: 0px 5px 0px 0px
+        box-shadow: 0 2px 4px 0 rgba(0,0,0,.15)
+</style>
