e04b4adbb2cb62517970a5da47f34164f33bf6d9
StewieButthurt <nosoff.slawa@yandex.ru>
2020-07-12 21:58:51 +0400

    Изменен заголовок страницы

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
     <div class="todo__container">
         <div class="todo__header">
             <div class="todo__header-title">
-                Todo List
+                Мои заметки
             </div>
         </div>
         <div class="todo__wrapper">
