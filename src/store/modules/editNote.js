51a524d05ff68b41f3b45fe3aabf2edb299fbb61
StewieButthurt <nosoff.slawa@yandex.ru>
2020-07-13 20:10:53 +0400

    Расширен функционал методов. Внедрение функциональности для записи истории действий.

--- src/store/modules/editNote.js
+++ src/store/modules/editNote.js
@@ -1,43 +1,26 @@
 const state = () => ({
-    editNote: false,
+    editNote: {},
     editNoteHistory: [],
     editNoteStatus: 0
 })
 
 const mutations = {
     clearNote(state) {
-        state.editNote = []
+        state.editNote = {}
         state.editNoteHistory = []
     },
     clearEditNoteHistory(state) {
         state.editNoteHistory = []
     },
     setNote(state, { todo, index }) {
-        state.editNote = todo
+        state.editNote = JSON.parse(JSON.stringify(todo))
         state.editNote.index = index
         state.editNoteHistory = []
     },
-    setStatus(state, { index, status, history }) {
-        console.log('setStatus')
+    setStatus(state, { index, status, history, commit }) {
         state.editNote.todo[index].status = status
 
         if (!history) {
-
-            if (state.editNoteStatus !== state.editNoteHistory.length) {
-
-                console.log('заменяем массив')
-                let arr = []
-                state.editNoteHistory.forEach((item, index) => {
-                    if (index <= state.editNoteStatus - 1) {
-                        arr.push(item)
-                    }
-                });
-
-                state.editNoteHistory = arr
-
-            }
-
-
             state.editNoteHistory.push({
                 methods: 'setStatus',
                 index: index,
@@ -49,14 +32,60 @@ const mutations = {
         }
 
     },
-    addTodo(state, title) {
+    addTodo(state, { title, history, index, back }) {
+
+        if (history && back) {
+            return state.editNote.todo.splice(index, 1)
+        }
+
         state.editNote.todo.push({
             status: false,
             title: title
         })
+
+        let newIndex = state.editNote.todo.length - 1
+
+        if (!history) {
+            state.editNoteHistory.push({
+                methods: 'addTodo',
+                newTitle: title,
+                index: newIndex
+            })
+
+            state.editNoteStatus++
+        }
     },
-    deleteTodo(state, index) {
+    deleteTodo(state, { title, index, history, back }) {
+
+        if (!history) {
+            var oldTitle = state.editNote.todo[index].title
+        }
+
+        if (history && back) {
+
+
+            state.editNote.todo.push({
+                status: false,
+                title: title
+            })
+
+            let newIndex = state.editNote.todo.length - 1
+            return state.editNoteHistory[state.editNoteStatus]
+                .index = newIndex
+        }
+
         state.editNote.todo.splice(index, 1)
+
+        if (!history) {
+
+            state.editNoteHistory.push({
+                methods: 'deleteTodo',
+                index: index,
+                oldTitle: oldTitle
+            })
+
+            state.editNoteStatus++
+        }
     },
     setTitleTodo(state, { index, title }) {
         state.editNote.todo[index].title = title
@@ -77,13 +106,13 @@ const actions = {
         commit('setNote', { todo, index })
     },
     async setStatus({ commit }, { index, status }) {
-        commit('setStatus', { index, status })
+        commit('setStatus', { index, status, commit })
     },
-    async addTodo({ commit }, title) {
-        commit('addTodo', title)
+    async addTodo({ commit }, { title }) {
+        commit('addTodo', { title })
     },
-    async deleteTodo({ commit }, index) {
-        commit('deleteTodo', index)
+    async deleteTodo({ commit }, { index }) {
+        commit('deleteTodo', { index })
     },
     async setTitleTodo({ commit }, { index, title }) {
         commit('setTitleTodo', { index, title })
@@ -94,31 +123,48 @@ const actions = {
     async returnEditBack({ state, commit }) {
         if (state.editNoteStatus !== 0) {
 
-            console.log(state.editNoteStatus)
+            let history = state.editNoteHistory[state.editNoteStatus - 1]
 
-            let methods = state.editNoteHistory[state.editNoteStatus - 1].methods
+            let methods = history.methods
 
-            let oldTitle = state.editNoteHistory[state.editNoteStatus - 1].oldTitle ?
-                state.editNoteHistory[state.editNoteStatus - 1].oldTitle :
-                false
+            let oldTitle = history.oldTitle
 
-            let oldStatus = state.editNoteHistory[state.editNoteStatus - 1].oldStatus ?
-                state.editNoteHistory[state.editNoteStatus - 1].oldStatus :
-                false
+            let oldStatus = history.oldStatus
 
-            let index = state.editNoteHistory[state.editNoteStatus - 1].index ?
-                state.editNoteHistory[state.editNoteStatus - 1].index :
-                false
+            let index = history.index
 
             commit('setEditNoteStatus', state.editNoteStatus - 1)
 
-            console.log(state.editNoteStatus)
+            commit(`${methods}`, {
+                history: true,
+                back: true,
+                index: index,
+                title: oldTitle,
+                status: oldStatus,
+            })
+
+        }
+    },
+    async returnEditForward({ state, commit }) {
+        if (state.editNoteStatus <= state.editNoteHistory.length - 1) {
+
+            let history = state.editNoteHistory[state.editNoteStatus]
+
+            let methods = history.methods
+
+            let newTitle = history.newTitle
+
+            let newStatus = history.newStatus
+
+            let index = history.index
+
+            commit('setEditNoteStatus', state.editNoteStatus + 1)
 
             commit(`${methods}`, {
                 history: true,
-                index: index ? index : false,
-                title: oldTitle ? oldTitle : false,
-                status: oldStatus ? oldStatus : false
+                index: index,
+                title: newTitle,
+                status: newStatus,
             })
 
         }
