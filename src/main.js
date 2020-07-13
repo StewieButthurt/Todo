a26094416358e69016a7848f196a7fa5d689b043
StewieButthurt <nosoff.slawa@yandex.ru>
2020-07-14 02:26:28 +0400

    Добавление метрики

--- src/main.js
+++ src/main.js
@@ -7,12 +7,20 @@ import Vuex from 'vuex';
 import store from './store'
 import router from './router';
 
+import VueYandexMetrika from 'vue-yandex-metrika'
+
 router.beforeEach((to, from, next) => {
     document.title = to.meta.title
     next()
 })
 
 Vue.use(Vuex);
+
+Vue.use(VueYandexMetrika, {
+    id: 65592406,
+    router: router,
+    env: process.env.NODE_ENV
+})
 
 new Vue({
     router,
